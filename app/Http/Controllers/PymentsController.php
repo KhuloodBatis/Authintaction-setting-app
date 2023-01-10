d9e3feb8dd3bfd56496a11f22e8baaf51993adfb
KhuloodBatis <k.batis@bimventures.com>
2023-01-10 15:57:12 +0300

    event & listaner

--- app/Http/Controllers/PymentsController.php
+++ app/Http/Controllers/PymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductPurchased;
 use App\Notifications\PaymentReceived;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
@@ -13,9 +14,16 @@ class PymentsController extends Controller
         return view('pyments.create');
     }
 
+    // public function store()
+    // {
+    //     // request()->user()->notify(new PaymentReceived(900));
+    //     Notification::send(request()->user(), new PaymentReceived(900));
+    // }
+
     public function store()
     {
-        // request()->user()->notify(new PaymentReceived(900));
-        Notification::send(request()->user(), new PaymentReceived(900));
+
+        ProductPurchased::dispatch('toy');
+
     }
 }
